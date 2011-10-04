e4307272bf25f629cdb2817f50effff1ece15ac5
dbonnie <dbonnie@ncsa.illinois.edu>
2011-10-04 12:49:06 -0500

    Changed existing RGB histogram descriptor and extractor to Pixel Histogram. Current implementation was not a true color histogram representation.

---
+++ src/main/java/edu/illinois/ncsa/versus/descriptor/impl/PixelHistogramDescriptor.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+package edu.illinois.ncsa.versus.descriptor.impl;
+
+import edu.illinois.ncsa.versus.descriptor.Feature;
+
+/**
+ * Binned color histogram.
+ * 
+ * @author Luigi Marini
+ * 
+ */
+public class PixelHistogramDescriptor implements Feature {
+
+	private final int numBins;
+
+	private final int[][][] histogram;
+	
+	//private final int[][][] normalizedHistogram;
+
+	public PixelHistogramDescriptor() {
+		this(16);
+		//TODO: should be changed to 256, the measures can deal with binning issues. 
+	}
+
+	public PixelHistogramDescriptor(int numBins) {
+		this.numBins   = numBins;
+		this.histogram = new int[numBins][numBins][numBins];
+		
+		//this.normalizedHistogram = getNormalizedHistogram( this.histogram );
+		
+	}
+
+	public void add(int r, int g, int b) {
+		int intervalSize = 256 / numBins;
+		int x = r / intervalSize;
+		int y = g / intervalSize;
+		int z = b / intervalSize;
+		histogram[x][y][z]++;
+	}
+
+	@Override
+	public String getType() {
+		return this.getClass().toString();
+	}
+
+	/**
+	 * @return the numBins
+	 */
+	public int getNumBins() {
+		return numBins;
+	}
+
+	/**
+	 * Normalize the input RGB histogram.
+	 * 
+	 * @param h
+	 *     Un-normalized histogram (RGB)
+	 * @return
+	 *     Normalized histogram (RGB)
+	 */    
+//	public int[][][] getNormalizedHistogram( int[][][] h ){
+//		
+//		int[][][] nH = new int[256][256][256];
+//		
+//		int minR = 0;
+//		int minG = 0;
+//		int minB = 0;
+//		
+//		return nH;
+//	}
+	
+	@Override
+	public String getName() {
+		return "Pixel Histogram";
+	}
+
+	@Override
+	public String toString() {
+		String s = "Showing contents of " + getName() + "\n";
+		for (int x = 0; x < numBins; x++) {
+			for (int y = 0; y < numBins; y++) {
+				for (int z = 0; z < numBins; z++) {
+					if (histogram[x][y][z] != 0) {
+						s += ("[" + x + ", " + y + ", " + z + "] = "
+								+ histogram[x][y][z] + "\n");
+					}
+				}
+			}
+		}
+		return s;
+	}
+
+	public int get(int x, int y, int z) {
+		return histogram[x][y][z];
+	}
+}
