1337d9a954ad68218a9228403752c4f6f6a5e4a6
dbonnie <dbonnie@ncsa.illinois.edu>
2011-10-11 13:17:24 -0500

    Quick fixes to the KL and Jeffrey measures. Added get methods for histograms (RGB, Grayscale, Pixel).

--- src/main/java/edu/illinois/ncsa/versus/measure/impl/KLdivergenceMeasure.java
+++ src/main/java/edu/illinois/ncsa/versus/measure/impl/KLdivergenceMeasure.java
@@ -12,7 +12,7 @@ import edu.illinois.ncsa.versus.measure.SimilarityPercentage;
 
 
 /**
- * Kullback-Leibler Divergence of two features.
+ * Kullback-Leibler Divergence of two features. The image histograms are modified s.t. they satisfy the requirements of a probability mass funtion.  
  * 
  * @author Devin Bonnie
  */
@@ -29,26 +29,38 @@ public class KLdivergenceMeasure implements Measure {
 	private SimilarityNumber compare( PixelHistogramDescriptor feature1, PixelHistogramDescriptor feature2 ) throws Exception {
 		
 		// Check feature lengths, they must be equal
-		if( feature1.getNumBins() != feature2.getNumBins() ) {
-			
+		if( feature1.getNumBins() != feature2.getNumBins() ) {			
 			throw new Exception("Features must have the same length");
 		}
-			
-		double dist = 0;
 		
-		for( int i=0; i < feature1.getNumBins(); i++ ){
-			
-			for( int j=0; j < feature1.getNumBins(); j++ ){
-				
+		int[][][] pHist1 = feature1.getHistogram(); 
+		int[][][] pHist2 = feature2.getHistogram(); 
+		double max1      = 0;
+		double max2      = 0;		
+		double dist      = 0;
+		
+		for( int i=0; i < feature1.getNumBins(); i++ ){			
+			for( int j=0; j < feature1.getNumBins(); j++ ){				
+				for( int k=0; k < feature1.getNumBins(); k++ ){	
+					max1 += feature1.get(i,j,k);
+					max2 += feature2.get(i,j,k);
+				}
+			}
+		}
+
+		for( int i=0; i < feature1.getNumBins(); i++ ){			
+			for( int j=0; j < feature1.getNumBins(); j++ ){				
 				for( int k=0; k < feature1.getNumBins(); k++ ){					
 					//ignore zero bins
 					if( (feature1.get(i,j,k) != 0.0) && (feature2.get(i,j,k) != 0.0) ){
-						dist += (double)feature1.get(i,j,k) * Math.log( (double)feature1.get(i,j,k) / (double)feature2.get(i,j,k) );
+						
+						double a = (double)feature1.get(i,j,k) / max1;
+						double b = (double)feature2.get(i,j,k) / max2;						
+						dist    += a * Math.log( a / b );
 					}
 				}
 			}	
 		}		
-		
 		return new SimilarityNumber(dist);
 	}
 		
@@ -62,26 +74,35 @@ public class KLdivergenceMeasure implements Measure {
 	 */	private SimilarityNumber compare( RGBHistogramDescriptor feature1, RGBHistogramDescriptor feature2 ) throws Exception {
 
 		// Check feature lengths, they must be equal
-		if( feature1.getNumBins() != feature2.getNumBins() ) {
-			
+		if( feature1.getNumBins() != feature2.getNumBins() ) {			
 			throw new Exception("Features must have the same length");
 		}
 		
 		int[][] normHist1 = feature1.computeNormalizedHistogram();
 		int[][] normHist2 = feature2.computeNormalizedHistogram();
 		
-		double dist = 0;
+		double max1[]     = {0,0,0}; 
+		double max2[]     = {0,0,0};
+		double dist       = 0;
 		
 		for( int i=0; i < normHist1[0].length; i++ ){
 			
+			for (int x=0; x < normHist1.length; x++){
+				max1[i] += normHist1[x][i];
+				max2[i] += normHist2[x][i];
+			}				
+				
 			for( int j=0; j < normHist1.length; j++ ){					
 				//ignore zero bins
 				if( (normHist1[j][i] != 0.0) && (normHist2[j][i] != 0.0) ){						
 					
-					dist += (double)normHist1[j][i] * Math.log( (double)normHist1[j][i] / (double)normHist2[j][i] );
-				}
-			}	
-		}	
+					double a = (double)normHist1[j][i] / max1[i];
+					double b = (double)normHist2[j][i] / max2[i];
+					
+					dist += a * Math.log(  a / b );
+				}			
+			}			
+		}
 		return new SimilarityNumber(dist);
 	}
 	
@@ -96,21 +117,30 @@ public class KLdivergenceMeasure implements Measure {
 	private SimilarityNumber compare( GrayscaleHistogramDescriptor feature1, GrayscaleHistogramDescriptor feature2 ) throws Exception {
 
 		// Check feature lengths, they must be equal
-		if( feature1.getNumBins() != feature2.getNumBins() ) {
-			
+		if( feature1.getNumBins() != feature2.getNumBins() ) {			
 			throw new Exception("Features must have the same length");
 		}
 		
 		int[] normHist1 = feature1.computeNormalizedHistogram();
 		int[] normHist2 = feature2.computeNormalizedHistogram();
+		double max1     = 0; 
+		double max2     = 0;
 		
+		for (int i=0; i < normHist1.length; i++){
+			max1 += normHist1[i];
+			max2 += normHist2[i];
+		}
+
 		double dist = 0;
 		
 		for( int j=0; j < normHist1.length; j++ ){					
 			//ignore zero bins
 			if( (normHist1[j] != 0.0) && (normHist2[j] != 0.0) ){						
 				
-				dist += (double)normHist1[j] * Math.log( (double)normHist1[j] / (double)normHist2[j] );
+				double a = (double)normHist1[j] / max1;
+				double b = (double)normHist2[j] / max2;
+				
+				dist += a * Math.log(  a / b );
 			}			
 		}			
 		return new SimilarityNumber(dist);
