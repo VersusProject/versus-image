2c203937c551dba2fc276839f7b67da640033f3c
dbonnie <dbonnie@ncsa.illinois.edu>
2011-11-07 15:21:12 -0600

    Added support for extracting Hough Lines (via javacv) and encapsulating the resulting data into a descriptor.

---
+++ src/main/java/edu/illinois/ncsa/versus/descriptor/impl/HoughLinesDescriptor.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package edu.illinois.ncsa.versus.descriptor.impl;
+
+import java.util.ArrayList;
+
+import edu.illinois.ncsa.versus.descriptor.Feature;
+import edu.illinois.ncsa.versus.descriptor.impl.Pixel;
+
+/**
+ * Binned pixel histogram.
+ * 
+ * @author Luigi Marini, Devin Bonnie
+ * 
+ */
+public class HoughLinesDescriptor implements Feature {
+
+	private ArrayList<Pixel[]> lines = new ArrayList<Pixel[]>();
+	private final int numLines;
+
+	public HoughLinesDescriptor(ArrayList<Pixel[]> input) {
+
+		this.lines    = input;
+		this.numLines = lines.size();
+	}
+
+	@Override
+	public String getType() {
+		return this.getClass().toString();
+	}
+	
+	@Override
+	public String getName() {
+		return "Hough Lines";
+	}
+
+	@Override
+	public String toString() {
+		String s = "Showing contents of " + getName() + "\n";
+		
+		for( int i=0; i<numLines; i++){
+			Pixel[] ll = lines.get(i);
+			s = s + "Line " + i + ": x1= " + ll[0].x() + " y1 = " + ll[0].y() + "-- x2= " + ll[1].x() + " y2 = " + ll[1].y() + "\n";
+		}		
+		return s;
+	}
+	
+}
